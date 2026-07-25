75fc46b86621d6bc44554712acd84b1fef969a6f
EJCP3 <euddycabrera03@gmail.com>
2026-07-25 17:24:29 -0400

    v17

---
+++ apps/docs/src/i18n/utils.ts
@@ -0,0 +1,20 @@
+import es from '../locales/es.json';
+import en from '../locales/en.json';
+
+export type Lang = 'es' | 'en';
+export const defaultLang: Lang = 'es';
+
+const ui = {
+  es,
+  en,
+} as const;
+
+export function useTranslations(lang: Lang) {
+  return function t(key: string) {
+    return key.split('.').reduce((obj: any, k: string) => (obj || {})[k], ui[lang]) || key;
+  };
+}
+
+export function getTranslations(lang: Lang, key: string) {
+  return key.split('.').reduce((obj: any, k: string) => (obj || {})[k], ui[lang]) || {};
+}
